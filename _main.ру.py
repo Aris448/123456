98dd7f996bed8c09c80096ee109cde8de90de11f
Aris448 <arismart.pro@gmail.com>
2024-09-29 15:14:30 +0300

    Add files via upload

---
+++ _main.ру.py
@@ -0,0 +1,8 @@
+print(" Приветствую тебя в игре-квиз!")
+points = 0
+if input("Земля круглая? ") == "да": points += 1
+else: points -= 1
+
+if input("20//4 == 5? ") == "да":points += 1
+else: points -= 1
+if input(" 2 + 2 == 4? ") == "да": points += 1
